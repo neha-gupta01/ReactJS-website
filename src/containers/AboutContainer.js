5f11ad83877e645bae9b1b09d7df83f7e13af89e
Balwant Singh <balwant.singh@deorwine.com>
2023-12-12 12:11:38 +0530

    Squashed commit of the following:
    commit 1b0985cc0f6e5853954f5e63aca51e8b75d68953
    Author: neha-gupta01 <nehagupta96365@gmail.com>
    Date:   Tue Dec 12 06:26:18 2023 +0530
    
        updated cart functionality quantity
    
    commit 4bef75b96523154dee774e6cf3426aa276222423
    Author: Balwant Singh <balwant.singh@deorwine.com>
    Date:   Mon Dec 11 13:17:06 2023 +0530
    
        separate context
    
    commit 0b164032a60663f0e4fe17b719b7ee3f78e01566
    Author: Balwant Singh <balwant.singh@deorwine.com>
    Date:   Mon Dec 11 13:07:19 2023 +0530
    
        added common context api

--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "../components/navbar";
 import About from "../components/about";
 import Footer from "../components/footer";
+import Cart from "../components/cart";
 
 const AboutContainer = () => {
   return (
     <React.Fragment>
       <Navbar />
+      <Cart />
       <About />
       <Footer />
     </React.Fragment>
